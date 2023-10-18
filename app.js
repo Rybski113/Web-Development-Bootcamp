0e972665312a152462abcdd2ed343e9aa808c4ee
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2023-10-18 20:51:51 +0200

    Update app.js
    if else statement

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 let n = Math.random()
-n = n *6
+n = n * 100
 n = Math.floor(n) + 1
 console.log(n)
+
+
+if ( n > 50) {
+    alert(n + '%' + 'He loves you!')
+} else {
+    alert(n + '%' + 'Try again !')
+}
