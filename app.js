1f599c2e92fb5c2badb85cf352c4e2c82207579d
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2023-10-19 19:59:00 +0200

    leap year chalenge
    if else method

---
+++ app.js
@@ -0,0 +1,15 @@
+function isLeap(year) {
+    if(year % 4 === 0) {
+        if(year % 100 === 0) {
+            if(year % 400 === 0) {
+                console.log('leap year')
+            }
+        } else {
+            console.log('leap year')
+        }
+    } else {
+        console.log(' not a leap year')
+    }
+}
+
+isLeap(2001)
